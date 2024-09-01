8a13657f72e6e331ce1d18a5f1abfcebae4e1db6
Engnzlz <engzlz@outlook.com>
2024-09-01 14:32:12 -0500

    Cuarto commit - interfaz mejorada

--- src/app/components/frx-calculator/frx-calculator.component.ts
+++ src/app/components/frx-calculator/frx-calculator.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CalculatorService } from '../../services/calculator.service';
 import { QuoteResponse } from '../../services/quote-response.interface';
-import { FormsModule } from '@angular/forms';  // Importa FormsModule
+import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,17 +14,35 @@ import { CommonModule } from '@angular/common';
 export class FrxCalculatorComponent {
   initialCurrency = 'COP';
   finalCurrency = 'USD';
-  initialAmount = 200000;
+  initialAmount: number | null = null;
   quote: QuoteResponse | undefined;
+  errorMessage: string = '';
+  minAmount: number = 1
 
   constructor(private calculatorService: CalculatorService) {}
 
   getQuote() {
-    this.calculatorService.getQuote(this.initialCurrency, this.finalCurrency, this.initialAmount)
-      .subscribe((response: QuoteResponse) => {
-        this.quote = response;
-      }, error => {
-        console.error('Error al obtener la cotización:', error);
-      });
+    if (this.validateAmount()) {
+      this.calculatorService.getQuote(this.initialCurrency, this.finalCurrency, this.initialAmount!)
+        .subscribe((response: QuoteResponse) => {
+          this.quote = response;
+        }, error => {
+          console.error('Error al obtener la cotización:', error);
+        });
+    }
+  }
+  validateAmount(): boolean {
+    if (this.initialAmount === null || this.initialAmount === undefined) {
+      this.errorMessage = 'Este campo es obligatorio.';
+      return false;
+    } else if (isNaN(this.initialAmount)) {
+      this.errorMessage = 'Debe ingresar un número válido.';
+      return false;
+    } else if (this.initialAmount < this.minAmount) {
+      this.errorMessage = `El monto debe ser mayor que ${this.minAmount}.`;
+      return false;
+    }
+    this.errorMessage = '';
+    return true;
   }
 }
